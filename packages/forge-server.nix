811935c923e47955bf57f7f7b209ced32e8aabd0
Edu Renesto <edure95@gmail.com>
2024-08-02 19:41:19 -0300

    pkg: use prebuilt tarball

--- packages/forge-server.nix
+++ packages/forge-server.nix
@@ -3,20 +3,21 @@
 , fetchurl
 , jre_headless
 , gnused
+, gnutar
 }: stdenv.mkDerivation rec {
   version = "1.20.1-47.3.0";
   name = "forge-server-${version}";
 
   src = fetchurl {
-    url = "https://github.com/EduRenesto/minecraft-forge-nix/raw/main/jars/forge-${version}-installer-offline.jar";
-    hash = "sha256-JYQXqJpsHKv4KtoDZs6i+r9F4vnMR0IvFvqV9Pt913g=";
+    url = "https://grytuiwswqfo.objectstorage.sa-saopaulo-1.oci.customer-oci.com/n/grytuiwswqfo/b/mc_public/o/rxnl-patotacraft-server-${version}.tar.gz";
+    hash = "sha256-4S5OFvMyg4ub6Zl7FMtl7P1iCLd9M6dOZw7Lts7+fug=";
   };
 
   dontUnpack = true;
 
   buildPhase = ''
     mkdir -p $out
-    ${jre_headless}/bin/java -jar $src --offline --installServer $out
+    ${gnutar}/bin/tar xvf $src -C $out
   '';
 
   installPhase = ''
